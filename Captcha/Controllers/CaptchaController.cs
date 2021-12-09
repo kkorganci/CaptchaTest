3741b32cc15a511c308d3c549ddc7e95a17b9989
Kahraman <kahramankorganci@gmail.com>
2021-12-09 21:54:00 +0300

    Add project files.

---
+++ Captcha/Controllers/CaptchaController.cs
@@ -0,0 +1,32 @@
+﻿using Captcha.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CaptchaMvc.HtmlHelpers;
+
+namespace Captcha.Controllers
+{
+    public class CaptchaController : Controller
+    {
+        // GET: Captcha
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Registration registration)
+        {
+            if (this.IsCaptchaValid("Captcha geçerlidir"))
+            {
+                ViewBag.Message = "Capctha geçerli, giriş başarılı";
+            }
+            else
+            {
+                ViewBag.Message = "Captcha geçersiz, giriş başarısız";
+            }
+            return View();
+        }
+    }
+}
